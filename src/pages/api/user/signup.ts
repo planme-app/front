45456553ba1ddbd58c71244317fd281ae2bb3e18
2heeesss <hohooodo@gmail.com>
2023-05-08 15:16:54 +0900

    [Feat] #21 add email check in signup API

--- src/pages/api/user/signup.ts
+++ src/pages/api/user/signup.ts
@@ -12,8 +12,13 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const userRepository = new PrismaUserRepository(prisma);
     const userService = new UserUseCase(userRepository);
     try {
-      const newUser = await userService.signup({ passwd, email, name });
-      res.status(201).json({ user: newUser });
+      const isAvailableEmail = await !userService.checkEmail(email);
+      if (isAvailableEmail) {
+        const newUser = await userService.signup({ passwd, email, name });
+        res.status(201).json({ user: newUser });
+      } else {
+        res.status(409).json({ error: 'User with this email already exists' });
+      }
     } catch (error) {
       if (error instanceof Error) {
         res.status(500).json({ error: error.message });
