cb113d6e9477f537065e7063eb72140b70237944
고범석 <kobums23@gmail.com>
2023-05-15 18:18:34 +0900

    [Feat] #31 add routine template page layout

---
+++ src/components/atoms/MainBody.tsx
@@ -0,0 +1,29 @@
+import React, { PropsWithChildren } from 'react';
+import { Container } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+const StyledRoot = styled('div')(({ theme }) => ({
+  [theme.breakpoints.up('md')]: {
+    display: 'flex'
+  }
+}));
+
+const StyledContent = styled('div')(() => ({
+  maxWidth: 375,
+  margin: 'auto',
+  minHeight: '100vh',
+  display: 'flex',
+  flexDirection: 'column'
+}));
+
+export default function MainBody({ children }: PropsWithChildren) {
+  return (
+    <>
+      <StyledRoot>
+        <Container maxWidth="sm">
+          <StyledContent sx={{ pt: 6 }}>{children}</StyledContent>
+        </Container>
+      </StyledRoot>
+    </>
+  );
+}
---
+++ src/pages/design/addpage.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import Head from 'next/head';
+
+import { Typography } from '@mui/material';
+import MainBody from 'components/atoms/MainBody';
+import RoutineTemplateCard from 'components/organisms/RoutineTemplateCard';
+
+export default function AddPage() {
+  const routineList = [
+    {
+      routineName: '팔굽혀',
+      imageSrc: '/icon/pushups72.png'
+    },
+    {
+      routineName: '걷기',
+      imageSrc: '/icon/pushups72.png'
+    },
+    {
+      routineName: '팔굽혀',
+      imageSrc: '/icon/pushups72.png'
+    },
+    {
+      routineName: '걷기',
+      imageSrc: '/icon/pushups72.png'
+    }
+  ];
+
+  const themeList = [
+    {
+      themeName: '운동',
+      routineList: routineList
+    },
+    {
+      themeName: '마음',
+      routineList: routineList
+    },
+    {
+      themeName: '공부',
+      routineList: routineList
+    }
+  ];
+
+  return (
+    <>
+      <Head>
+        <title>addpage</title>
+      </Head>
+      <MainBody>
+        <Typography
+          sx={{
+            display: 'inline',
+            fontWeight: 'bold',
+            fontSize: 30
+          }}
+        >
+          습관 선택
+        </Typography>
+        {themeList.map((theme, index) => (
+          <RoutineTemplateCard
+            routineTheme={theme.themeName}
+            routineList={theme.routineList}
+            key={index}
+          />
+        ))}
+      </MainBody>
+    </>
+  );
+}
