94fc5f5e337f89fba0d0e1e2d5fddfb371139ed1
2heeesss <hohooodo@gmail.com>
2023-05-19 22:26:28 +0900

    [Fix] camelcase to snakecase

---
+++ prisma/migrations/20230519132540_fix_camelcase_to_snakecase/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `User`;
+
+-- CreateTable
+CREATE TABLE `user` (
+    `user_id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `passwd` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `user_email_key`(`email`),
+    PRIMARY KEY (`user_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
