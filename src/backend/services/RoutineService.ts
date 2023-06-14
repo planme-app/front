5d9ce1d8cd46ca29c18843c90c7e3305ce305cdf
2heeesss <hohooodo@gmail.com>
2023-06-15 00:45:49 +0900

    [Feat] add new routine service

--- src/backend/services/RoutineService.ts
+++ src/backend/services/RoutineService.ts
@@ -1,14 +1,86 @@
-import { Routine } from 'models/Routine';
+import { Routine, RoutineInstanceWithGoal } from 'models/Routine';
 import { RoutineRepository } from 'repositories/RoutineRepository';
 import { RoutineInstanceRepository } from 'repositories/RoutineInstanceRepository';
 import { utcToZonedTime } from 'date-fns-tz';
 import { getDay } from 'date-fns';
+import { RoutineType as PrismaRoutineType } from '@prisma/client';
 
 export class RoutineUseCase {
   constructor(
     private routineRepository: RoutineRepository,
     private routineInstanceRepository: RoutineInstanceRepository
   ) {}
+
+  async addRoutine(
+    userId: string,
+    title: string,
+    type: PrismaRoutineType,
+    daysOfWeek: string[],
+    goal: string
+  ) {
+    // 1. create routine
+    const days = ['일', '월', '화', '수', '목', '금', '토'];
+    const daysOfWeekBinary = days
+      .map((day) =>
+        daysOfWeek.find((inputDay) => inputDay === day) ? '1' : '0'
+      )
+      .join('');
+
+    const routine = await this.routineRepository.addRoutine(
+      userId,
+      title,
+      type,
+      daysOfWeekBinary
+    );
+
+    // 2. create routine_instance
+    const routineInstance =
+      await this.routineInstanceRepository.addRoutineInstance(
+        routine.routine_id
+      );
+
+    let routineTypeInstance: RoutineInstanceWithGoal;
+    // 3. create type_routine_instance
+    switch (routine.type) {
+      case 'time':
+        routineTypeInstance =
+          await this.routineInstanceRepository.addTimeRoutineInstance(
+            routineInstance.routine_instance_id,
+            Number(goal)
+          );
+        break;
+
+      case 'count':
+        routineTypeInstance =
+          await this.routineInstanceRepository.addCountRoutineInstance(
+            routineInstance.routine_instance_id,
+            Number(goal)
+          );
+        break;
+
+      case 'bool':
+        routineTypeInstance =
+          await this.routineInstanceRepository.addBoolRoutineInstance(
+            routineInstance.routine_instance_id,
+            goal === 'true'
+          );
+        break;
+      default:
+        throw new Error('routine의 타입이 정확하지 않습니다.');
+    }
+
+    const res = {
+      routine_instance_id: routineInstance.routine_instance_id,
+      created_at: routineInstance.created_at,
+      title: routine.title,
+      type: routine.type,
+      days_of_week: daysOfWeek,
+      goal: routineTypeInstance.goal,
+      progress: routineTypeInstance.progress
+    };
+
+    return res;
+  }
 
   async getTodayRoutine(userId: string) {
     const day = ['일', '월', '화', '수', '목', '금', '토'];
