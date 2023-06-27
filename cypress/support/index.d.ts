772072a91d254335fadb3ea6613bd48ac7ed6e55
2heeesss <hohooodo@gmail.com>
2023-06-27 19:11:23 +0900

    [Chore] #66 add get data-cy command in cypress

---
+++ cypress/support/index.d.ts
@@ -0,0 +1,11 @@
+export {};
+
+export {};
+
+declare global {
+  namespace Cypress {
+    interface Chainable<Subject> {
+      getDataCy(selector: string, ...args: any[]): Chainable<Subject>;
+    }
+  }
+}
